82cbd92e32627f8f75ed0f8b5b116d6e5de63d49
Anthony Yung <88053027+sudocraft@users.noreply.github.com>
2021-10-03 08:22:36 +0800

    Update entrypoint.sh

--- .github/actions/deploy/entrypoint.sh
+++ .github/actions/deploy/entrypoint.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 
-ansible-playbook -i ./.github/actions/deploy/hosts \
+mkdir ~/.ssh
+echo $VAULT_PASS > /vault_password_file.txt
+
+ansible-vault decrypt \
+--vault-password-file=/vault_password_file.txt \
+./.ssh/id_rsa-encrypted \
+--output=~/.ssh/id_rsa
+
+chmod 0600 ~/.ssh/id_rsa
+
+ansible-playbook --vault-password-file=/vault_password_file.txt \
+-i ./.github/actions/deploy/hosts \
 ./.github/actions/deploy/deploy.yml
